ba5d995a17db5ff090f065f0ad9f29debed36a2b
Kindhearthomecareservices <steve.toroitich@gmail.com>
2026-09-25 15:15:36 +0300

    Update config.js

--- portal/config.js
+++ portal/config.js
@@ -2,6 +2,6 @@ const KH_CONFIG = {
   SUPA_URL: "https://enlvelqeqbmhamyqtxzj.supabase.co",
   SUPA_KEY: "sb_publishable_PIHnwkRKsXvvJjnVQRLx5g_l5SAXbQw",
   ADMIN_PASS: "KindHeart@2024",
-  COLORS: { blue:"#14495f", green:"#5ca94a", bg:"#f8faf6" }
+  WA: "254707546365"
 };
 const supa = supabase.createClient(KH_CONFIG.SUPA_URL, KH_CONFIG.SUPA_KEY);
