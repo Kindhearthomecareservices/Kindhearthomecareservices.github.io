15c573abf8afb708e678c044755fdf65e370189e
Kindhearthomecareservices <steve.toroitich@gmail.com>
2026-09-26 13:35:22 +0300

    Delete portal/config.js

--- portal/config.js
+++
@@ -1,4 +0,0 @@
-let status = 'Normal';
-let sBP = parseInt(bp.value.split('/')[0]||0);
-if(sBP>=180||parseFloat(temp.value)>=38||parseInt(spo2.value)<90) status='HIGH ALERT';
-else if(sBP>=140||parseFloat(temp.value)>=37.5) status='Elevated';
